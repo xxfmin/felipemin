6ac79d6922773402a36cd4ad5f651cb835a90f8d
Felipe Min <felipemin03@gmail.com>
2025-07-21 17:13:59 -0400

    tweaked footer

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -11,8 +11,8 @@ export default function Footer() {
   }
 
   return (
-    <footer className="relative md:fixed bottom-0 left-0 right-0 bg-background text-gray-400 text-xs border-t border-gray-800 z-20">
-      <div className="flex justify-center items-center px-6 py-4">
+    <footer className="relative md:fixed bottom-0 left-0 right-0 text-gray-400 text-xs border-gray-800 z-20">
+      <div className="flex justify-end items-center px-6 py-4">
         <span>© 2025 made by felipe min</span>
       </div>
     </footer>
